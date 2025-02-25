d539452023b5e9cad3d0846740ae6a3d79fef004
Angel Diaz Rodriguez <miguel0221lol@gmail.com>
2025-02-25 12:20:09 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Guarderia guarderia = new Guarderia();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            String[] options = {"Agregar Estudiante", "Mostrar Estudiantes", "Mostrar Mayores de 5 años", "Salir"};
+            int eleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción", "Menú", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+            if (eleccion == 0) {
+                String id = JOptionPane.showInputDialog("Ingrese ID:");
+                String nombre = JOptionPane.showInputDialog("Ingrese Nombre:");
+                int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Edad:"));
+                String genero = JOptionPane.showInputDialog("Ingrese Género:");
+                String documento = JOptionPane.showInputDialog("Ingrese Documento de Identidad:");
+                String alergias = JOptionPane.showInputDialog("Ingrese Alergias:");
+                String nombreAcudiente = JOptionPane.showInputDialog("Ingrese Nombre del Acudiente:");
+                String numAcudiente = JOptionPane.showInputDialog("Ingrese Número de Contacto del Acudiente:");
+
+                Estudiante nuevoEstudiante = new Estudiante(id, nombre, edad, genero, documento, alergias, nombreAcudiente, numAcudiente);
+                guarderia.agregarEstudiante(nuevoEstudiante);
+            } else if (eleccion == 1) {
+                guarderia.mostrarListaEstudiantes();
+            } else if (eleccion == 2) {
+                guarderia.mostrarMayoresDeCinco();
+            } else {
+            }
         }
     }
 }
